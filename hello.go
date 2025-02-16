9f32751bea840ba4f0d5981f0c12918ba28b6a77
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-02-16 23:15:54 +0300

    Add comments to hello.go for clarity

--- hello.go
+++ hello.go
@@ -1,7 +1,8 @@
-package main
+package main //define package
 
-import "fmt"
+import "fmt" //import fmt package for printing
 
+//below is a funtion called main that uses the imported fmt package to print a message
 func main()  {
 	fmt.Println("Hello World")
 }
