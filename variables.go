a66d2a4325b3dfd15dd316840388491135745f52
Malcom Otieno Adrian <malcolmotieno16@gmail.com>
2025-02-17 00:13:37 +0300

    Add variables.go with initial variable declarations and print statement

---
+++ variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var name string = "Malcom Otieno"
+	age := 20
+	var is_new bool = true
+	var pi float64 = 3.14
+
+
+	fmt.Println(name, age, is_new, pi)
+}
